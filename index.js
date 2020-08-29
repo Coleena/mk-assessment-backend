0529e594aec26c37c97faa0397a6dc0eaec61f81
Coleen Wu <cow013@ucsd.edu>
2020-08-28 17:36:05 -0700

    test

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000
 
+const { Client } = require('pg');
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+client.connect();
+
 app.use(express.json());
 
 app.get('/prices', (req, res) => {
     res.send("Get request: /prices");
+
+    client.query('SELECT * FROM items;', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+            console.log(JSON.stringify(row));
+        }
+        client.end();
+    });
 });
 app.get('/prices/:iname', (req, res) => {
     res.send(`Get request: /prices/${req.params.iname}`);
